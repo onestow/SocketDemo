86427b30057a07db7df66ad30bc37ebbb75adb20
tom <811515556@qq.com>
2017-07-17 15:29:43 +0800

    new

---
+++ Client/Client.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Client
+{
+    class Client
+    {
+        static Socket clientSocket;
+        static void Main()
+        {
+            byte[] result = new byte[1024];
+            //设定服务器IP地址  
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                clientSocket.Connect(new IPEndPoint(ip, 1234)); //配置服务器IP与端口  
+                Console.WriteLine("连接服务器成功");
+            }
+            catch
+            {
+                Console.WriteLine("连接服务器失败，请按回车键退出！");
+                return;
+            }
+
+            //通过clientSocket接收数据  
+            //int receiveLength = clientSocket.Receive(result);
+            //Console.WriteLine("接收服务器消息：{0}", Encoding.UTF8.GetString(result, 0, receiveLength));
+            //通过 clientSocket 发送数据  
+            while(true)
+            { 
+                try
+                {
+                    //Thread.Sleep(10);
+                    string sendMessage = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffff");
+                    clientSocket.Send(Encoding.UTF8.GetBytes(sendMessage));
+                    Console.WriteLine("向服务器发送消息：" + sendMessage);
+                }
+                catch
+                {
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                    break;
+                }
+            }
+            Console.WriteLine("发送完毕，按回车键退出");
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    class Server
+    {
+        static Socket serverSocket;
+        public static ManualResetEvent TimeOutObject = new ManualResetEvent(false);
+        static bool isRecSuc = false;
+        static int len;
+        static void Main()
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+             serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);  
+            serverSocket.Bind(new IPEndPoint(ip, 1234));  //绑定IP地址：端口  
+            serverSocket.Listen(10);    //设定最多10个排队连接请求  
+            Console.WriteLine("启动监听{0}成功", serverSocket.LocalEndPoint.ToString());  
+            //通过Clientsoket发送数据  
+            Thread myThread = new Thread(ListenClientConnect);  
+            myThread.Start();  
+            Console.ReadLine();  
+
+        }
+
+        private static void ListenClientConnect()  
+        {  
+            while (true)  
+            {  
+                Socket clientSocket = serverSocket.Accept();  
+                clientSocket.Send(Encoding.ASCII.GetBytes("Welcome"));  
+                //Thread receiveThread = new Thread(ReceiveMessage);  
+                Thread receiveThread = new Thread(RecMsgThread);  
+                receiveThread.Start(clientSocket);  
+            }  
+        }
+
+
+
+        private static void RecMsgThread(object obj)
+        {
+            Socket socket = obj as Socket;
+            socket.BeginReceive(result, 0, 1024, SocketFlags.None, RecMsgCallBack, socket);
+            while(true)
+            {
+                TimeOutObject.Reset();
+                if(TimeOutObject.WaitOne(2000, false))
+                {
+                    if(isRecSuc)
+                    {
+                        Console.WriteLine(Encoding.ASCII.GetString(result, 0, len));
+                    }
+                    else
+                    {
+                        Console.WriteLine("disconnect");
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("timeout");
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                    return;
+                }
+            }
+        }
+
+        private static void RecMsgCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                Socket socket = ar.AsyncState as Socket;
+                len = socket.EndReceive(ar);
+                if (len <= 0) isRecSuc = false;
+                else
+                {
+                    //Console.WriteLine("************  " + Encoding.ASCII.GetString(result, 0, len));
+                    isRecSuc = true;
+                    socket.BeginReceive(result, 0, result.Length, SocketFlags.None, RecMsgCallBack, socket);
+                }
+            }
+            catch (SocketException ex)
+            {
+                isRecSuc = false;
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                isRecSuc = false;
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                TimeOutObject.Set();
+            }
+        }
+
+        static byte[] result = new byte[1024];
+    }
+}
